44a950656fa5f5bd2e1b5980bbf3c8e1ace9da7b
tania-official <>
2026-08-08 06:11:28 +0000

    Challenge: Product Management Capstone

--- conditional_statements/challenge_product_management_capstone/main.py
+++ conditional_statements/challenge_product_management_capstone/main.py
@@ -5,7 +5,7 @@ product_type = "Perishable" # Can be "Perishable" or "Non-Perishable"
 if product_type == "Perishable":
     if days_until_expiration <= 3 and stock_level > 50:
          print("30% discount applied")
-    elif days_until_expiration > 4 and days_until_expiration < 6 and stock_level > 50:
+    elif days_until_expiration >= 4 and days_until_expiration <= 6 and stock_level > 50:
         print("20% discount applied")
     elif days_until_expiration > 6 and stock_level <= 50:
         print("10% discount applied")
