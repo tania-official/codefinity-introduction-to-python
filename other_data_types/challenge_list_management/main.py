d74668586bba1ec2cf112851c4152f3a7451fc86
tania-official <>
2026-08-09 12:58:00 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,12 @@
+meat = ["Ham",3.99,50,"Sliced"]
+cheese = ["Cheddar",5.49,100,"Sharp"]
+condiment = ["Mustard",1.99,75,"Spicy"]
+deli_dept = [meat,cheese,condiment]
+print ("Initial Deli List:", deli_dept)
+if meat[0] == "Ham" and meat[2]< 100:
+    meat[2] =100
+seasonal_meat = ["Turkey",4.50,100,"Sliced"]
+deli_dept.append (seasonal_meat)
+deli_dept.remove (deli_dept[2])
+deli_dept.sort ()
+print ("Updated Deli List:",deli_dept)
